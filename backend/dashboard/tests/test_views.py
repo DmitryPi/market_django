451f693e9d88a0db95c7ad35d934a68b9dd4f01a
Dmitry G <sdprecovery@gmail.com>
2023-02-23 11:03:30 +0300

    Tests for DashboardSettingsView

--- backend/dashboard/tests/test_views.py
+++ backend/dashboard/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
 
@@ -9,13 +10,13 @@ class DashboardViewTests(TestCase):
         self.user = UserFactory()
         self.url = reverse("board:index", kwargs={"username": self.user.username})
 
-    def test_response(self):
+    def test_get(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "dashboard/index.html")
 
-    def test_response_anon(self):
+    def test_get_anon(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
 
@@ -25,12 +26,47 @@ class DashboardSettingsViewTests(TestCase):
         self.user = UserFactory()
         self.url = reverse("board:settings")
 
-    def test_response(self):
+    def test_get(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "dashboard/settings.html")
 
-    def test_response_anon(self):
+    def test_get_anon(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
+
+    def test_post_avatar_form(self):
+        self.client.force_login(self.user)
+        # read mock image
+        with open("backend/dashboard/tests/test_avatar.jpg", "rb") as f:
+            image = SimpleUploadedFile(
+                "test_avatar.jpg", f.read(), content_type="image/jpeg"
+            )
+        data = {"avatar": image}
+        response = self.client.post(reverse("dashboard:settings"), data, follow=True)
+        self.user.refresh_from_db()
+        # tests
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard/settings.html")
+        self.assertIn("avatar_form", response.context)
+        self.assertEqual(self.user.avatar, "avatars/test_avatar.jpg")
+
+    def test_post_avatar_form_invalid(self):
+        self.client.force_login(self.user)
+        # read mock image
+        data = {"avatar": ""}
+        response = self.client.post(reverse("dashboard:settings"), data, follow=True)
+        self.user.refresh_from_db()
+        # tests
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard/settings.html")
+        self.assertIn("avatar_form", response.context)
+        self.assertEqual(self.user.avatar, "avatars/default.png")
+
+    def test_post_avatar_form_anon(self):
+        data = {"avatar": ""}
+        response = self.client.post(reverse("dashboard:settings"), data, follow=True)
+        # tests
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/login.html")
