f59031e0058fb62c9aea605387135b87675f98a5
Dmitry G <sdprecovery@gmail.com>
2023-02-27 11:49:56 +0300

    Add PageDetailView + urls; Tests for PageDetailView;

--- backend/pages/urls.py
+++ backend/pages/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
 from .apps import PagesConfig
-from .views import AboutView, PolicyView
+from .views import PageDetailView
 
 app_name = PagesConfig.verbose_name
 
 urlpatterns = [
-    path("about/", AboutView.as_view(), name="about"),
-    path("policy/", PolicyView.as_view(), name="policy"),
+    path("<str:slug>/", PageDetailView.as_view(), name="page-detail"),
 ]
--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,9 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
+
+from .models import Page
 
 
-class AboutView(TemplateView):
-    template_name = "pages/about.html"
-
-
-class PolicyView(TemplateView):
-    template_name = "pages/policy.html"
+class PageDetailView(DetailView):
+    model = Page
+    template_name = "pages/detail.html"
+    slug_url_kwarg = "slug"
